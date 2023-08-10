a15f16225001716df3f63656a3e83048c18b1cd9
lihaixu <939415835@qq.com>
2023-08-10 15:52:16 +0800

    bean的生命周期（10步法）： 1，实例化bean 2,bean属性赋值 3,bean名字：flowerBean 3,类加载器：jdk.internal.loader 3，bean工厂: root of factory 4，bean后处理器，before方法执行，开始初始化 5.afterPropertiesSet执行 6，初始化bean 7,bean后处理器，after方法执行，bean初始化完成 8,执行bean 9,diDisposableBean destroy 10销毁bean

---
+++ springtest2/src/main/java/com/springtest2/day06/beanlive10/Flower.java
@@ -0,0 +1,64 @@
+package com.springtest2.day06.beanlive10;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+
+/**
+ * Author:李海旭
+ * Date : 2023/8/10 15:19
+ */
+public class Flower implements BeanNameAware, BeanClassLoaderAware, BeanFactoryAware, InitializingBean, DisposableBean {//bean生命周期10步法，实现多个接口
+    private String name;
+
+    public Flower() {
+        System.out.println("1，实例化bean");
+    }
+
+    public Flower(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        System.out.println("2,bean属性赋值");
+    }
+    public void initBean(){
+        System.out.println("6，初始化bean");
+    }
+
+    public void destoryBean(){
+        System.out.println("10销毁bean");
+    }
+
+    @Override
+    public String toString() {
+        return "Flower{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public void setBeanClassLoader(ClassLoader classLoader) {
+        System.out.println("3,类加载器："+classLoader);
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("3，bean工厂:"+beanFactory);
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("3,bean名字："+name);
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("9,diDisposableBean destroy");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("5.afterPropertiesSet执行");
+    }
+}
---
+++ springtest2/src/main/java/com/springtest2/day06/beanlive10/Log.java
@@ -0,0 +1,22 @@
+package com.springtest2.day06.beanlive10;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+/**
+ * Author:李海旭
+ * Date : 2023/8/10 15:24
+ */
+public class Log implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("5，bean后处理器，before方法执行，开始初始化");
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("7,bean后处理器，after方法执行，bean初始化完成");
+        return bean;
+    }
+}
